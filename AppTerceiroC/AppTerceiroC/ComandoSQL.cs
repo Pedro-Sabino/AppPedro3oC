ca732cb6be70a296aef574ed2d102a9bd9f96612
aluno <aluno@L03MICRO02>
2023-10-09 10:13:22 -0300

    Add project files.

---
+++ AppTerceiroC/AppTerceiroC/ComandoSQL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTerceiroC
+{
+    public class ComandoSQL
+    {
+        public static string inserirNome = ""
+    }
+}
